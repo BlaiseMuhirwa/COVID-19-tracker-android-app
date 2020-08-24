d94228466aa1602c907a33761e9561b2a354c372
Blaise Munyampirwa <blaisemuhirwa3@gmail.com>
2020-08-24 11:54:26 -0500

    add model statistics class

--- app/src/main/java/com/blaisemuhirwa/covid_19trackerandroidapp/Home.java
+++ app/src/main/java/com/blaisemuhirwa/covid_19trackerandroidapp/Home.java
@@ -14,9 +14,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 
 public class Home extends Fragment {
@@ -32,6 +36,7 @@ public class Home extends Fragment {
     private TextView recordedDeathTollTextView;
     private TextView newDeathsTextView;
     private TextView totalRecoveredTextView;
+    private TextView newRecoveredTextView;
 
     public Home() {
 
@@ -56,10 +61,17 @@ public class Home extends Fragment {
         recordedDeathTollTextView = view.findViewById(R.id.recordedDeathTollTextView);
         newDeathsTextView = view.findViewById(R.id.newDeathsTextView);
         totalRecoveredTextView = view.findViewById(R.id.totalRecoveredTextView);
+        newRecoveredTextView = view.findViewById(R.id.newRecoveredTextView);
 
         progressBar.setVisibility(View.GONE);
-
+        loadHomeData();
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadHomeData();
     }
 
     private void loadHomeData() {
@@ -82,12 +94,50 @@ public class Home extends Fragment {
                  */
                 progressBar.setVisibility(View.GONE);
                 Toast.makeText(context, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
-
             }
         });
+
+        /* add requests to queue */
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
+
     }
 
     private void handleJSONResponse(String response) {
+        try {
+            /* Create a JSON object from the response we get using the api
+                Required: response
+                Created:
+                    - new confirmed cases
+                    - total confirmed cases
+                    - new deaths
+                    - total deaths
+                    - new recovered
+                    - total recovered
+            */
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject globalObject = jsonObject.getJSONObject("Global");
+            String newConfirmedCases = globalObject.getString("NewConfirmed");
+            String totalConfirmedCases = globalObject.getString("TotalConfirmed");
+            String newDeaths = globalObject.getString("NewDeaths");
+            String totalDeaths = globalObject.getString("TotalDeaths");
+            String totalRecovered = globalObject.getString("TotalRecovered");
+            String newRecovered = globalObject.getString("NewRecovered");
 
+            totalRecordedCasesTextView.setText(totalConfirmedCases);
+            newCasesTextView.setText(newConfirmedCases);
+            recordedDeathTollTextView.setText(totalDeaths);
+            newDeathsTextView.setText(newDeaths);
+            totalRecoveredTextView.setText(totalRecovered);
+            newRecoveredTextView.setText(newRecovered);
+
+            /* hide progress bar */
+            progressBar.setVisibility(View.GONE);
+
+        }
+        catch (Exception exception) {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(context, ""+exception.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
