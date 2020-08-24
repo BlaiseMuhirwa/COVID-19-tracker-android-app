94de0aea60bc236188dd2f1242f2cd4e3af31df4
Blaise Munyampirwa <blaisemuhirwa3@gmail.com>
2020-08-24 00:57:07 -0500

    Change MainActivity to respond to on-click events for bottom navigation

--- app/src/main/java/com/blaisemuhirwa/covid_19trackerandroidapp/MainActivity.java
+++ app/src/main/java/com/blaisemuhirwa/covid_19trackerandroidapp/MainActivity.java
@@ -1,14 +1,49 @@
 package com.blaisemuhirwa.covid_19trackerandroidapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener{
+    /* User-Interface */
+    private BottomNavigationView navigationView;
+    private ImageButton refreshButton;
+    private TextView titleTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* initialize user-interface views */
+        navigationView = findViewById(R.id.navigationView);
+        refreshButton = findViewById(R.id.refreshButton);
+        titleTextView = findViewById(R.id.titleTextView);
+
+        /* refresh records on click event */
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        /* handle bottom navigation item clicks */
+
+        return false;
     }
 }
